60aac7354f4e84eefe20655ef17ed02043a24b91
harshedabdulla <harshed2004@gmail.com>
2022-12-29 17:17:37 +0530

    homepage complete

--- src/components/LocationBox/Locationbox.jsx
+++ src/components/LocationBox/Locationbox.jsx
@@ -17,23 +17,23 @@ function LocationBox() {
     <div className='mapBox'>
     <div className="relative rounded-md shadow-sm">
     
-      <div className="px-4 py-5 bg-white sm:p-6">
-        <h3 className="text-2xl font-bold leading-7 text-gray-900">
+      <div className="px-4 py-5 sm:p-6">
+        <h3 className="text-2xl font-bold leading-9 text-white-1000">
           Your location
         </h3>   
         <div className="mt-2">
           {latitude && longitude ? (
-            <p className="text-base leading-6 text-gray-500">
+            <p className="text-base leading-8 text-white-800">
               Latitude: {latitude}, Longitude: {longitude}
             </p>
           ) : (
-            <p className="text-base leading-6 text-gray-500">
+            <p className="text-base leading-8 text-white-800">
               Loading location...
             </p>
           )}
         </div>
       </div>
-      <div className="px-4 py-3 bg-gray-50 sm:px-6 sm:py-4 sm:flex sm:justify-end">
+      <div className="px-4 py-3 sm:px-6 sm:py-4 sm:flex sm:justify-end">
         <a
           href={`/map?lat=${latitude}&lng=${longitude}`}
           className="inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-800"
