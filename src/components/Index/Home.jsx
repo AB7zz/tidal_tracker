1d7079c2638c5e89c22dec52a017c8f55262e65d
ab7z <abhiksi613@gmail.com>
2022-12-29 17:39:22 +0530

    copy pasted Harsheds code because I dont know how to merge

--- src/components/Index/Home.jsx
+++ src/components/Index/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import * as React from 'react';
+import TopNav from '../TopNavbar/TopNav';
+import WeatherCard from '../WeatherCard/WeatherCard';
+import LocationBox from '../LocationBox/Locationbox';
+import { Link } from 'react-router-dom';
 
-const Home = () => {
+function Home(){
+
   return (
-    <div>Home</div>
-  )
+    <>
+      <TopNav/>
+      <div className="Weatherbox">
+        <Link to='/weatherDetails'>
+          <WeatherCard temperature={72} description="Sunny Day" icon="03d" location="Thotappally" date="29/12/2022" wind="3 km/h" precipitation="13%" humidity="60%" predicted="37-40 C Safe to Go"/>
+        </Link>
+      </div>
+      <LocationBox></LocationBox>
+    </>
+  );
 }
-
 export default Home
