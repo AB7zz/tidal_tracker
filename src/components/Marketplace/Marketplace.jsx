319a4c7f154c8a3fce9ec419ca9450c9e6a4545b
harshedabdulla <harshed2004@gmail.com>
2022-12-30 07:27:50 +0530

    completed marketplace

--- src/components/Marketplace/Marketplace.jsx
+++ src/components/Marketplace/Marketplace.jsx
@@ -4,9 +4,10 @@ import EachProduct from '../EachProduct/EachProduct';
 const Marketplace = () => {
   return (
     <div>
-        <h2 className='text-4xl font-bold'>Marketplace</h2>
-        <EachProduct name="Fibre Boat" price={50} owner="Harshed" imageUrl="https://5.imimg.com/data5/JM/IP/MY-7547396/fishing-fiber-big-boat-500x500.jpg" />
-        <EachProduct name="Net" price={75} owner="Sameer" imageUrl="https://5.imimg.com/data5/JM/IP/MY-7547396/fishing-fiber-big-boat-500x500.jpg" />  
+        <h2 className='text-4xl font-bold ml-10'>Marketplace</h2>
+        <EachProduct name="Fibre Boat" price={50} owner="Harshed" imageUrl="http://www.outdeck.com/images/BIC-Boats/With-Logo/310-02-fibre-fishing-boat-price.jpg" />
+        <EachProduct name="Net" price={75} owner="Sameer" imageUrl="https://images.unsplash.com/photo-1538904029080-68e0f3167224?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmlzaGluZyUyMG5ldHxlbnwwfHwwfHw%3D&w=1000&q=80" /> 
+        <EachProduct name="Net" price={75} owner="Sameer" imageUrl="https://images.unsplash.com/photo-1538904029080-68e0f3167224?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmlzaGluZyUyMG5ldHxlbnwwfHwwfHw%3D&w=1000&q=80" />   
     </div>
   )
 }
--- src/components/eachProduct/eachProduct.jsx
+++ src/components/eachProduct/eachProduct.jsx
@@ -2,15 +2,19 @@ import React, { useState, useEffect } from 'react';
 
 function EachProduct({ name, price, owner, imageUrl }) {
   return (
-    <div className="flex items-center">
+    
+    <div className="flex productbox">
     <div className="mr-4 px-3">
       <h2 className="text-xl">{name}</h2>
       <p>Price: {price}</p>
       <p>Owner: {owner}</p>
-      <button className='btn-primary'>Rent Product</button>
+      <button className="bg-red">Rent Product</button>
     </div>
-    <img src={imageUrl} className="w-16 h-16 px-20"/>
+    <div className='ml-15'>
+    <img src={imageUrl} className="w-20 h-20"/>
+    </div>
   </div>
+  
   );
 }
 export default EachProduct
